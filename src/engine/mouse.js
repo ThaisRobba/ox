816db3a212963efdce369af24f099e059c8e5145
OttoRobba <ottorobba@gmail.com>
2015-04-19 16:58:18 -0300

    removed mouse move function call for scene - mouse.x and y is already set on ox.mouse

--- src/engine/mouse.js
+++ src/engine/mouse.js
@@ -8,14 +8,14 @@ module.exports = {
     onMove: function (e) {
         ox.mouse.x = e.clientX - ox.canvas.offsetLeft;
         ox.mouse.y = e.clientY - ox.canvas.offsetTop;
-        if (scene.current.mouseMove) scene.current.mouseMove(ox.mouse);
     },
+
     onUp: function (e) {
         if (scene.current.mouseUp) scene.current.mouseUp(this.buttons[e.button]);
         this.isDown = false;
     },
-    onDown: function (e) {
 
+    onDown: function (e) {
         if (scene.current.mouseDown) scene.current.mouseDown(this.buttons[e.button]);
         this.isDown = true;
     },
